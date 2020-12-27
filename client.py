0d404efc6d2012614ed1488655942eed660f5db6
Changxing <jimmy75792@gmail.com>
2020-12-27 14:59:01 +1300

    initial commit

---
+++ client.py
@@ -0,0 +1,146 @@
+"""
+A program as a client to request for date or time
+
+Author: Changxing Gong
+Date: 09/Aug/2020
+"""
+import socket, sys, select
+WAITING_TIME = 1
+
+def args_checker():
+    """Check argument number and check if they are valid
+       if they are valid then return (request, (host, port))
+    """
+    message = ''
+    if len(sys.argv) == 4:
+        if sys.argv[1] not in ["date", "time"]:
+            message += 'First argument: "date" or "time" only\n\n'
+        try:
+            address = None
+            conn = socket.getaddrinfo(sys.argv[2], None)
+            for family, kind, _, _, addr in conn:
+                if family == socket.AddressFamily.AF_INET and kind == socket.SocketKind.SOCK_DGRAM:
+                    address = addr
+            if address is None:
+                raise TypeError
+        except:
+            message += 'Second argument: invalid IP address or domain name\n\n'
+        try:
+            if int(sys.argv[3]) > 64000 or int(sys.argv[3]) < 1024:
+                message += 'Third argument: invalid port number (must between 1024 and 64000 inclusive)\n\n'
+        except:
+            message += 'Third argument: not an integer (must between 1024 and 64000 inclusive)\n\n'
+        if message != '':
+            print(message)
+            sys.exit('Invalid arguments')
+        else:
+            message += 'Sending request...\n'
+            print(message)
+            return sys.argv[1], (address[0], int(sys.argv[3]))
+    else:
+        message += "To request a date-time packet, you need exactly 3 arguments: [] [] [] \n\n"
+        message += 'Request type: "date" or time\n\n'
+        message += 'IP address(e.g. "000.00.00.000") or server name(e.g. "datetime.example.nz")\n\n'
+        message += 'Port number: [1024, 64000]\n\n'
+        print(message)
+        sys.exit('Invalid arguments')
+
+def get_request_pkt(request):
+    """Prepare request packet"""
+    magic_num = [0x49, 0x7E]
+    pkt_type = [0, 1]
+    request_type = [0, request]
+    buffer = magic_num + pkt_type + request_type
+    return bytearray(buffer)
+
+def parse_pkt(data):
+    """Parse incoming packet, exit the program if it's unexpected"""
+    if len(data) < 13:
+        print("Unexpected packet: too few bytes recevied")
+        sys.exit()
+    magicNo = (data[0] << 8) + data[1]
+    pkt_typ = (data[2] << 8) + data[3]
+    lang_code = (data[4] << 8) + data[5]
+    year = (data[6] << 8) + data[7]
+    month, day, hour, minute = data[8], data[9], data[10], data[11]
+    length = data[12]
+    if magicNo != 0x497E:
+        print("Unexpected packet: magic number not match")
+        raise TypeError
+    elif pkt_typ != 0x0002: #Response pkt
+        print("Unexpected packet: Not a reponse packet")
+        raise TypeError
+    elif lang_code not in [0x0001, 0x0002, 0x0003]: #Eng, Mao , Ger
+        print("Unexpected packet: Can not recognise language contained")
+        raise TypeError
+    elif year >= 2100:
+        print("Unexpected date: not a valid year")
+        raise TypeError
+    elif month not in range(1, 13):
+        print("Unexpected date: not a valid month")
+        raise TypeError
+    elif day not in range(1, 32):
+        print("Unexpected date: not a valid day")
+        raise TypeError
+    elif hour not in range(0, 24):
+        print("Unexpected time: not a valid hour")
+        raise TypeError
+    elif minute not in range(0, 60):
+        print("Unexpected time: not a valid minute")
+        raise TypeError
+    elif length != len(data[13:]):
+        print("Unexpected length: actual packet length not match")
+        raise TypeError
+    else:
+        if lang_code == 0x1:
+            lang = "English"
+        elif lang_code == 0x2:
+            lang = "Te reo Maori"
+        else:
+            lang = "German"
+        print(f"MagicNo: {hex(magicNo)}")
+        print(f"Packet type: {hex(pkt_typ)}")
+        print(f"language code: {hex(lang_code)} ({lang})")
+        print(f"Year: {year}")
+        print(f"Month: {month}")
+        print(f"Day: {day}")
+        print(f"Hour: {hour}")
+        print(f"Minute: {minute}")
+        print(f"Textual representation: \"{data[13:].decode('utf-8')}\"\n\n")
+
+def wait_for_response(sock, address):
+    """Wait for server to respond
+       WAITING_TIME default to 1 second
+    """
+    global WAITING_TIME
+    readable, _, _ = select.select([sock], [], [], WAITING_TIME)
+    if len(readable) == 0:
+        sock.close()
+        sys.exit(f"Timeout: No response from {address}")
+    else:
+        data, address = sock.recvfrom(1024)
+        print(f"Receiving from {address}\n")
+        return data
+
+def main():
+    """Main function"""
+    request, address = args_checker()
+    request = 1 if request == "date" else 2
+    request_pkt = get_request_pkt(request)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        sock.sendto(request_pkt, address)
+        data = wait_for_response(sock, address)
+        parse_pkt(data)
+    except OSError:
+        print("Failed to send: Invalid address\n")
+        sock.close()
+        sys.exit("Invalid arguments")
+    except TypeError:
+        sys.exit("Can't parse the packet")
+    else:
+        sock.close()
+        sys.exit("Succcess")
+
+
+main()
---
+++ server.py
@@ -0,0 +1,169 @@
+"""
+A program as a server to receive request from clients
+
+Author: Changxing Gong
+Date: 09/Aug/2020
+"""
+import socket, sys, select, datetime
+HOST = '' #Automatically assigned to this device
+PORT_ENG = 0    #Command argument
+PORT_MAORI = 0  #Command argument
+PORT_GER = 0    #Command argument
+
+DATE_TEMPLATE = {}
+TIME_TEMPLATE = {}
+MONTH = {}
+
+def set_datetime_template():
+    """Set templates"""
+    global PORT_ENG, PORT_MAORI, PORT_GER, DATE_TEMPLATE, TIME_TEMPLATE, MONTH
+
+    DATE_TEMPLATE = {PORT_ENG : "Today’s date is {1} {0}, {2}",
+                 PORT_MAORI : "Ko te ra o tenei ra ko {1} {0}, {2}",
+                 PORT_GER : "Heute ist der {0}. {1} {2}"}
+
+    TIME_TEMPLATE = {PORT_ENG : "The current time is {0}:{1}",
+                 PORT_MAORI :"Ko te wa o tenei wa {0}:{1}",
+                 PORT_GER : "Die Uhrzeit ist {0}:{1}"}
+
+    MONTH = {PORT_ENG:{1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 
+                       6:"June", 7:"July", 8:"August", 9:"September", 10:"October",
+                       11:"November", 12:"December"},
+
+             PORT_MAORI:{1:"Kohitatea", 2:"Hui-tanguru", 3:"Poutu-te-rangi",
+                         4:"Paenga-whawha ", 5:"Haratua", 6:"Pipiri",
+                         7:"Hongongoi", 8:"Here-turi-koka", 9:"Mahuru",
+                         10:"Whiringa-a-nuku", 11:"Whiringa-a-rangi", 12:"Hakihea"},
+
+             PORT_GER:{1:"Januar", 2:"Februar", 3:"Marz", 4:"April", 5:"Mai", 6:"Juni",
+                       7:"Juli", 8:"August", 9:"September", 10:"Oktober", 11:"November",
+                       12:"Dezember"}
+             }
+
+def args_checker():
+    """Check argument number and check if they are valid
+       if they are valid then return (port1, port2, port3)
+    """
+    if len(sys.argv) == 4:
+        try:
+            port1 = int(sys.argv[1])
+            port2 = int(sys.argv[2])
+            port3 = int(sys.argv[3])
+        except ValueError:
+            print("Port numbers should be integer! [1024, 64000]\n")
+            sys.exit("Invalid arguments")
+        else:
+            if port1 not in range(1024, 64001):
+                print("Port number not in [1024, 64000]\n")
+                sys.exit("Invalid arguments")
+            if port2 not in range(1024, 64001):
+                print("Port number not in [1024, 64000]\n")
+                sys.exit("Invalid arguget_response_pktments")
+            if port3 not in range(1024, 64001):
+                print("Port number not in [1024, 64000]\n")
+                sys.exit("Invalid arguments")
+            else:
+                return port1, port2, port3
+    else:
+        print("Three port numbers required [] [] []\n")
+        sys.exit("Invalid arguments")
+
+def get_response_pkt(typ, port):
+    """Prepare response packet"""
+    global PORT_ENG, PORT_MAORI, PORT_GER, DATE_TEMPLATE, TIME_TEMPLATE, MONTH
+    magic_num = [0x49, 0x7E]
+    pkt_type = [0, 2]
+    if port == PORT_ENG:
+        language_code = [0, 0x1]
+    elif port == PORT_MAORI:
+        language_code = [0, 0x2]
+    elif port == PORT_GER:
+        language_code = [0, 0x3]
+    time = datetime.datetime.now().strftime("%d/%m/%Y/%H/%M")
+    day, month, year, hour, minute = [int(num) for num in time.split('/')]
+    buffer = magic_num + pkt_type + language_code + [year>>8, year&0b11111111, month, day, hour, minute, 0]
+    if typ == 1:   #date
+        text = DATE_TEMPLATE[port].format(day, MONTH[port][month], year)
+    else:          #time
+        if len(str(minute)) == 1:
+            minute = '0' + str(minute)
+        if len(str(hour)) == 1:
+            hour = '0' + str(hour)
+        text = TIME_TEMPLATE[port].format(hour, minute)
+    datetime_buffer = bytearray(text.encode('utf-8'))
+    if len(datetime_buffer) > 255:
+        raise ValueError
+    else:
+        buffer[-1] = len(datetime_buffer)
+        return bytearray(buffer) + datetime_buffer
+
+
+def parse_pkt(data):
+    """Parse incoming packet, drop the packet if it's unexpected"""
+    if len(data) != 6:
+        print("Unexpected packet length\n")
+        raise TypeError
+    elif (data[0] << 8) + data[1] != 0x497E:
+        print("Unexpected MagicNo\n")
+        raise TypeError
+    elif (data[2] << 8) + data[3] != 1:
+        print("Unexpected packet type\n")
+        raise TypeError
+    else:
+        typ = (data[4] << 8) + data[5]
+        if typ not in [1, 2]:
+            print("Unexpected request type\n")
+            raise TypeError
+        return typ
+
+def listening_loop(sock_eng, sock_maori, sock_ger):
+    """infinite loop, not consuming CPU resource whlie waiting for request"""
+    while True:
+        print("Waiting for the next event....\n")
+        readable, writble, excep = select.select([sock_eng, sock_maori, sock_ger], [], [],)
+        for sock in readable:
+            data, connection = sock.recvfrom(1024)
+            try:
+                typ = parse_pkt(bytearray(data))
+                pkt = get_response_pkt(typ, sock.getsockname()[1])
+            except TypeError:
+                print(f"Unexpected packet from {connection}: Discard\n")
+            except ValueError:
+                print("Failed datetime_bufferto response: Too large text to carry\n")
+            else:
+                typ = "date" if typ == 1 else "time"
+                print(f"A packet from {connection} requests for {typ}\n")
+                sock.sendto(pkt, connection)
+                print(f"Responded Successfully\n\n\n")
+
+def main():
+    """Main function"""
+    global HOST, PORT_ENG, PORT_MAORI, PORT_GER
+    PORT_ENG, PORT_MAORI, PORT_GER = args_checker()
+    set_datetime_template()
+    sock_eng = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock_maori = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock_ger = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    try:
+        sock_eng.bind((HOST, PORT_ENG))
+        sock_maori.bind((HOST, PORT_MAORI))
+        sock_ger.bind((HOST, PORT_GER))
+        print("Device name: " + socket.gethostname())
+        print("IP address of this device: " + socket.gethostbyname(socket.gethostname()+'.local'))
+        print(f"Assigned port for English: {PORT_ENG}")
+        print(f"Assigned port for Te reo Maori: {PORT_MAORI}")
+        print(f"Assigned port for German: {PORT_GER}\n")
+        listening_loop(sock_eng, sock_maori, sock_ger)
+    except OSError:
+        sock_eng.close()
+        sock_maori.close()
+        sock_ger.close()
+        sys.exit("Can't bind these port numbers")
+    except:
+        sock_eng.close()
+        sock_maori.close()
+        sock_ger.close()
+        sys.exit("\n\nStop listening")
+
+main()
